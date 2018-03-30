314f3ca59333ab19a5f679b0021e59343b11269d
Daniel Martinez Martinez <danieluned@hotmail.com>
2018-03-30 20:35:04 +0200

    angle

--- practica3/P3.cpp
+++ practica3/P3.cpp
@@ -20,6 +20,7 @@
 using namespace cv;
 using namespace std;
 
+#define PI 3.14159265
 
 void filtroGauss(Mat src, float t) {
 	MatIterator_<Vec3b> it, end;
@@ -58,12 +59,10 @@ int main(int argc, char * argv[]) {
 	imshow("Imagen", image);
 	
 	filtroGauss(image, 1);
+	
+
+	
 	//Para sobel https://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/sobel_derivatives/sobel_derivatives.html
-
-	//Finish program
-	std::cout << "Pulsa una tecla para terminar ";
-	cv::waitKey(0);
-
 	Mat src, src_gray;
 	Mat grad;
 	string window_name = "Sobel Demo - Simple Edge Detector";
@@ -86,30 +85,44 @@ int main(int argc, char * argv[]) {
 	/// Convert it to gray
 	cvtColor(src, src_gray, CV_BGR2GRAY);
 
-	/// Create window
-	namedWindow(window_name, CV_WINDOW_AUTOSIZE);
-
 	/// Generate grad_x and grad_y
 	Mat grad_x, grad_y;
 	Mat abs_grad_x, abs_grad_y;
-
+	Mat abs_grad_x2, abs_grad_y2;
 	/// Gradient X
 	//Scharr( src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
 	Sobel(src_gray, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT);
-	convertScaleAbs(grad_x, abs_grad_x);
-
+	
+	convertScaleAbs(grad_x, abs_grad_x,0.5,128);
+	convertScaleAbs(grad_x, abs_grad_x2);
 	imshow("Gx", abs_grad_x);
+
 	/// Gradient Y
 	//Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
-	Sobel(src_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT);
-	convertScaleAbs(grad_y, abs_grad_y);
+	Sobel(src_gray, grad_y, ddepth, 0, 1, 3, -scale, delta, BORDER_DEFAULT);
+	convertScaleAbs(grad_y, abs_grad_y,0.5,128);
+	convertScaleAbs(grad_y, abs_grad_y2);
 	imshow("Gy", abs_grad_y);
+	
 	/// Total Gradient (approximate)
-	addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
+	
+	addWeighted(abs_grad_x2, 0.5, abs_grad_y2, 0.5, 0, grad);
+	imshow("Magnitud del Gradiente", grad);
 
-	imshow(window_name, grad);
+	Mat angulo = grad_x.clone();
+	for (int i = 0; i < angulo.rows * angulo.cols; i++)
+	{
+		double directionRAD = atan2(grad_y.data[i], grad_x.data[i]);
 
-	waitKey(0);
+		angulo.data[i] = directionRAD * 128 / PI;
+	}
+	imshow("Angulo", angulo);
+	
+	
+
+	//Finish program
+	std::cout << "Pulsa una tecla para terminar ";
+	cv::waitKey(0);
 
 	return 0;
 }
